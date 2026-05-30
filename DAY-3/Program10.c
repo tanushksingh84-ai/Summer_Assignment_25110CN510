f3335a9ac88fe8f683680596a429fa8bd3ba0b6b
TANUSHK <tanushksingh84@gmail.com>
2026-05-30 11:59:40 +0530

    DAY-3 Uploaded.

---
+++ DAY-3/Program10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,i,j,flag;
+    printf("Enter range: ");
+    scanf("%d %d",&n1,&n2);
+    printf("Prime numbers between %d and %d are: ",n1,n2);
+    for(i=n1;i<=n2;i++)
+    {
+        if(i<=1) continue;   // skip 0 and 1
+        flag=0;
+        for(j=2;j<=i/2;j++)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag==0)
+            printf("%d ",i);
+    }
+    return 0;
+}
---
+++ DAY-3/Program12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,i,lcm;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a*b;i++)         // loop to find first common multiple
+    {     
+        if(i%a==0 && i%b==0)
+        {
+            lcm=i;
+            break;
+        }
+    }
+    printf("LCM of %d and %d is: %d",a,b,lcm);
+    return 0;
+}

32c28a5d564200f2cc5ba04ae7a6e86aab267b72
TANUSHK <tanushksingh84@gmail.com>
2026-06-04 10:28:40 +0530

    Day-8 uploaded

---
+++ DAY-8/Program31.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    char ch;
+    printf("Enter number of rows: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        ch='A';
+        for(j=1;j<=i;j++)
+        {
+            printf("%c",ch);
+            ch++;
+        }
+        printf("\n");
+    }
+    return 0;
+}

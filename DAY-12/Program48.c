8add22071b8e80fb8139f77396f08fe3a0c65d57
TANUSHK <tanushksingh84@gmail.com>
2026-06-12 12:59:26 +0530

    DAY-12 Uploaded.

---
+++ DAY-12/Program48.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int perfect(int n)
+{
+    int i,sum=0;
+    for(i=1;i<n;i++)
+        if(n%i==0)
+            sum=sum+i;
+    if(sum==n)
+        return 1;
+    else
+        return 0;
+}
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    if(perfect(n))
+        printf("Number is perfect");
+    else
+        printf("Number is not perfect");
+    return 0;
+}

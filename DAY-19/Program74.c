0ee8d4ef7e16db80de84620ba83abc8752155fec
TANUSHK <tanushksingh84@gmail.com>
2026-06-19 10:59:25 +0530

    DAY-19 Uploaded

---
+++ DAY-19/Program74.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int r, c, i, j;
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+    int a[r][c], b[r][c], sub[r][c];
+    printf("Enter first matrix:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter second matrix:\n");
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for(i = 0; i < r; i++)
+        for(j = 0; j < c; j++)
+            sub[i][j] = a[i][j] - b[i][j];
+
+    printf("Subtraction of matrices:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++)
+            printf("%d ", sub[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
